9022ec4764c372843f1bed816893400c1a1aa5d6
Idan Haitner <idanhaitner@gmail.com>
2023-04-12 19:13:52 +0300

    add docker

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const http = require('http');
 
 const { connectToMongo } = require('./services/mongo');
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 8000
 const server = http.createServer(app);
 
 async function startServer() {
